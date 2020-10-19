99f9a26d30ae42c12b811916b0c671cc75e02140
Nour Shams <nour0shams@outlook.com>
2020-10-19 22:04:00 +0300

    exit command

--- tasks.js
+++ tasks.js
@@ -34,7 +34,7 @@ function startApp(name){
  * @returns {void}
  */
 function onDataReceived(text) {
-  if (text === 'quit\n') {
+  if (text === 'quit\n' || text === 'exit\n') {
     quit();
   }
   else if(text === 'hello\n'){
@@ -77,6 +77,7 @@ function quit(){
   console.log('Quitting now, goodbye!')
   process.exit();
 }
+
 
 // The following line starts the application
 startApp("Nour Shams")
